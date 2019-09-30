c3055d8d8c3d54f00cab16611c97c2451b56dbfc
Yury Kurlykov <sh1ftr@protonmail.ch>
2019-10-01 00:47:52 +1000

    Comment out main.cpp file (as @DwarfMason requested)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-#include <processenv.h>
+#include <filesystem>
 
 int main(int argc, char *argv[]) {
+    auto kal = std::filesystem::current_path();
+    /*
+    TODO: переписать на std::filesystem
     FILE *config;
     config = fopen("config", "r+");
     if (config == nullptr){
@@ -9,5 +12,6 @@ int main(int argc, char *argv[]) {
         fwrite(argv[0], sizeof(*argv[0]), strlen(argv[0]) - 9, config);
     }
     fclose(config);
+    */
     return 0;
 }
