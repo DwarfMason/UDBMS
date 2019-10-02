17758ee4cd9612ea05d03d9c931dabe575169596
DwarfMason <igor2199@gmail.com>
2019-10-03 04:07:03 +1000

    implement testing system

--- main.cpp
+++ main.cpp
@@ -2,14 +2,22 @@
 #include <fstream>
 #include <filesystem>
 #include "commands.cpp"
-#include "storage_engine.h"
 
 
 int main() {
+    FILE *config;
+    config = fopen("config", "r+");
+    if (config == nullptr) {
+        config = fopen("config", "w+");
+        fclose(config);
+        std::ofstream out("config");
+        std::string path = std::filesystem::current_path();
+        out << path;
+        out.close();
+    }
     while (1){
         std::string str;
         std::getline(std::cin,str);
-        std::cout << str;
         parse_request(str.c_str());
     }
 }
