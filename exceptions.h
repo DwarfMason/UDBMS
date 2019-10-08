a93c6a63292afea3e536a775db5991415a75a92d
Yury Kurlykov <sh1ftr@protonmail.ch>
2019-10-09 01:58:04 +1000

    Improve exceptions' structure

--- exceptions.h
+++ exceptions.h
@@ -5,22 +5,31 @@
 
 struct sql_error : public std::runtime_error
 {
-    sql_error(const char* msg)
+    explicit sql_error(uint16_t errcode, const std::string& msg)
         : runtime_error(msg)
-    {}
+    {
+        error_code_ = errcode;
+        msg_ = msg;
+    }
+    virtual const char* what() const noexcept
+    {
+        return ("Error " + std::to_string(error_code_) + ": " + msg_).c_str();
+    }
+    uint16_t error_code_ = 0;
+    std::string msg_;
 };
 
 struct table_not_exist_error: public sql_error
 {
     table_not_exist_error()
-        : sql_error("Table does not exist")
+        : sql_error(100, "Table does not exist")
     {}
 };
 
 struct table_exist_error: public sql_error
 {
     table_exist_error()
-        : sql_error("Table does already exist")
+        : sql_error(101, "Table does already exist")
     {}
 };
 
