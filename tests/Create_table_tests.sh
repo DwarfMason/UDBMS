5606a23bc36b25623c1ee455b73c245ffab83562
DwarfMason <igor2199@gmail.com>
2019-10-09 03:29:47 +1000

    refactor testing system

--- tests/Create_table_tests.sh
+++ tests/Create_table_tests.sh
@@ -3,28 +3,29 @@
 cd ..
 cd ./cmake-build-debug/
 
-rm ../tests/Create_table_res
-touch ../tests/Create_table_res
+touch ../tests/Create_table_res_ok
+touch ../tests/Create_table_res_err
 
 
-exec 3>&1 1>> ../tests/Create_table_res
-exec 4>&2 2>> ../tests/Create_table_res
+exec 3>&1 1>> ../tests/Create_table_res_ok
+exec 4>&2 2>> ../tests/Create_table_res_err
 
 if ./UDBMS< ../tests/Create_table_input
 then
 	exec 1>&3
-	exec 2>&4
+	exec 2>&4 
 fi
 
 str_cnt=1
 ok_cnt=0
 
 while read LINE; do
-    buff=`head -n $str_cnt ../tests/Create_table_sol | tail -n +$str_cnt`
-	if [ "$buff" == "$LINE" ]
+    res_ok=`head -n $str_cnt ../tests/Create_table_res_ok | tail -n +$str_cnt`
+    res_err=`head -n $str_cnt ../tests/Create_table_res_err | tail -n +$str_cnt`
+	if [ "$res_ok" == "$LINE" ] | [ "$res_err" == "$LINE" ]
 	then
 		ok_cnt=$(( $ok_cnt + 1 )) 
-		echo 'OK' >> report
+		echo 'Test' "$str_cnt" 'passed' >> report
 	else
 		echo '---------------------------------' >> report
     	echo 'Test' "$str_cnt" 'failed' >> report
@@ -32,7 +33,10 @@ while read LINE; do
     	echo '---------------------------------' >> report
 	fi
 	str_cnt=$(( $str_cnt + 1 ))
-done < ../tests/Create_table_res
+done < ../tests/Create_table_sol
+
+rm ../tests/Create_table_res_ok
+rm ../tests/Create_table_res_err
 
 echo $ok_cnt'/'$(( $str_cnt - 1 )) 'tests passed' >> report
 
