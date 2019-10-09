feb0b5e950de7b963cf9a35623f00ae14d97dbfe
DwarfMason <igor2199@gmail.com>
2019-10-09 23:37:07 +1000

    hot fix

--- tests/Create_table_tests.sh
+++ tests/Create_table_tests.sh
@@ -17,21 +17,30 @@ then
 fi
 
 str_cnt=1
+res_ok_num=1
+res_err_num=1
 ok_cnt=0
 
 while read LINE; do
-    res_ok=`head -n $str_cnt ../tests/Create_table_res_ok | tail -n +$str_cnt`
-    res_err=`head -n $str_cnt ../tests/Create_table_res_err | tail -n +$str_cnt`
-	if [ "$res_ok" == "$LINE" ] | [ "$res_err" == "$LINE" ]
+    res_ok=`head -n $res_ok_num ../tests/Create_table_res_ok | tail -n +$res_ok_num`
+    res_err=`head -n $res_err_num ../tests/Create_table_res_err | tail -n +$res_err_num`
+	if [ "* $res_ok" == "$LINE" ] | [ "$res_err" == "$LINE" ]
 	then
 		ok_cnt=$(( $ok_cnt + 1 )) 
 		echo 'Test' "$str_cnt" 'passed' >> report
 	else
 		echo '---------------------------------' >> report
     	echo 'Test' "$str_cnt" 'failed' >> report
-    	echo 'Expected:' $buff $'\n''Got:' $LINE >> report
+    	echo 'Expected:' $LINE $'\n''Got:' $res_ok '/' $res_err >> report
     	echo '---------------------------------' >> report
 	fi
+	echo "${LINE:0:2}"
+	#if [ "${LINE:0:2}" == "* " ]
+	#	then 
+	#		$res_ok_num=$(( $res_ok_num + 1 ))
+	#	else
+	#		$res_err_num=$(( $res_err_num + 1 ))
+		fi
 	str_cnt=$(( $str_cnt + 1 ))
 done < ../tests/Create_table_sol
 
