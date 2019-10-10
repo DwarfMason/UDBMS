e2f6db5e4cb1f94d5b301e3f1ef71d2dd739224e
DwarfMason <igor2199@gmail.com>
2019-10-10 10:15:17 +1000

    test Lexer and create errors

--- tests/Lexer_tests.sh
+++ tests/Lexer_tests.sh
@@ -24,22 +24,22 @@ ok_cnt=0
 while read LINE; do
     res_ok=`head -n $res_ok_num ../tests/Lexer_tests_res_ok | tail -n +$res_ok_num`
     res_err=`head -n $res_err_num ../tests/Lexer_tests_res_err | tail -n +$res_err_num`
-	if [ "* $res_ok" == "$LINE" ] | [ "$res_err" == "$LINE" ]
+	if [ "= $res_ok" == "$LINE" ] || [ "$res_err" == "$LINE" ]
 	then
 		ok_cnt=$(( $ok_cnt + 1 )) 
 		echo 'Test' "$str_cnt" 'passed' >> report
 	else
 		echo '---------------------------------' >> report
     	echo 'Test' "$str_cnt" 'failed' >> report
-    	echo 'Expected:' $LINE $'\n''Got:' $res_ok '/' $res_err >> report
+    	echo 'Expected:' $LINE $'\n''Got:' "=" $res_ok '|' $res_err >> report
     	echo '---------------------------------' >> report
 	fi
 	
-	if [ "${LINE:0:2}" == "* " ]
+	if [ "${LINE:0:2}" == "= " ]
 		then 
-			$res_ok_num=$(( $res_ok_num + 1 ))
+			res_ok_num=$(( $res_ok_num + 1 ))
 		else
-			$res_err_num=$(( $res_err_num + 1 ))
+			res_err_num=$(( $res_err_num + 1 ))
 	fi
 	
 	str_cnt=$(( $str_cnt + 1 ))
